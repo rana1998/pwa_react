a42ec86660c19961a990938f948ffe912789d210
rana1998 <2015Kshitij14@gmail.com>
2021-06-27 09:14:24 +0530

    ff

---
+++ public/serviceworker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'version-1';
+const ulrsToCache = ['index.html','offline.html'];
+
+
+const self = this;
+//Install SW
+self.addEventListener('install', (event)=>{
+
+});
+//Listen for requests
+self.addEventListener('fetch', (event)=>{
+    
+});
+
+//Activste the SW
+self.addEventListener('active', (event)=>{
+    
+});
